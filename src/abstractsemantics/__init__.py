ae5799ecec03867afe10e0529a368ab79f9bbe4e
Laurent-Philippe Albou <24249870+lpalbou@users.noreply.github.com>
2026-01-17 09:38:25 +0100

    Initial Commit

---
+++ src/abstractsemantics/__init__.py
@@ -0,0 +1,12 @@
+from .registry import (
+    SemanticsRegistry,
+    load_semantics_registry,
+    resolve_semantics_registry_path,
+)
+
+__all__ = [
+    "SemanticsRegistry",
+    "load_semantics_registry",
+    "resolve_semantics_registry_path",
+]
+
